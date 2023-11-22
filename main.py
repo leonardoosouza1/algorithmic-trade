58cc9c59b6c8a3977b4165c3a63f8ed13f4f15a9
Leonardo <leonardo.vilson@hotmail.com>
2023-11-22 11:41:27 -0300

    refactor: add candlesticks

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import json
 from datetime import datetime, timedelta
 from analysis import data_fetch, trend_analysis
 from strategies import entry_exit_strategies, risk_management
-from utils import config, logger
+from utils import logger
+import matplotlib.pyplot as plt
+import pandas as pd
+import mplfinance as mpf
 
 
 def main():
@@ -33,6 +36,9 @@ def main():
         logger.log_entry_exit_signals(
             entry_signals, exit_signals, position_size)
 
+        # Plotting the historical data with indicators, entry, and exit signals
+        plot_data(trend_data, entry_signals, exit_signals)
+
         # Save aggregated signals to a JSON file (optional)
         signals_data = {
             "entry_signals": entry_signals.to_dict(orient='records'),
@@ -46,6 +52,60 @@ def main():
 
     except Exception as e:
         logger.error(f"Error processing {ticker}: {str(e)}")
+
+
+def plot_data(data, entry_signals, exit_signals):
+    # Convert 'Date' to datetime format
+    data['Date'] = pd.to_datetime(data['Date'])
+    data.set_index('Date', inplace=True)  # Set 'Date' column as the index
+
+    # Create addplot objects
+    ma_short_addplot = mpf.make_addplot(
+        data['MA_Short'], color='blue', linestyle='--')
+    ma_long_addplot = mpf.make_addplot(
+        data['MA_Long'], color='orange', linestyle='--')
+
+    # Entry and exit signals
+    entry_addplot = mpf.make_addplot(
+        entry_signals['Close'],
+        scatter=True,
+        color='g',
+        marker='^',
+        markersize=100,
+        secondary_y=True,
+    )
+
+    exit_addplot = mpf.make_addplot(
+        exit_signals['Close'],
+        scatter=True,
+        color='r',
+        marker='v',
+        markersize=100,
+        secondary_y=True,
+    )
+
+    # Candlestick chart
+    mpf.plot(
+        data,
+        type='candle',
+        addplot=[ma_short_addplot, ma_long_addplot],
+        show_nontrading=True,
+        style='yahoo',
+        title='Candlestick Chart with Entry and Exit Signals',
+        ylabel='Price',
+    )
+
+    # Entry and exit signals on a separate axis
+    mpf.plot(
+        data,
+        type='line',
+        addplot=[entry_addplot, exit_addplot],
+        show_nontrading=True,
+        style='yahoo',
+        ylabel='Signal',
+    )
+
+    plt.show()
 
 
 if __name__ == "__main__":
